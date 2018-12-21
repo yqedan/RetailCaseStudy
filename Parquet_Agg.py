6dfc7a19eac12930c4b5d2d6e8659f89cdf2c9dd
Yusuf Qedan <yusuf9191@gmail.com>
2018-12-21 15:16:39 -0800

    add simple agg functions

--- Parquet_Agg.py
+++ Parquet_Agg.py
@@ -1,23 +1,16 @@
 from pyspark.sql import SparkSession
-import sys
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+#from pyspark.sql.Column import *
+#from pyspark.sql.functions import *
 
-# Run script by using:
-# spark-submit --packages mysql:mysql-connector-java:5.1.38,com.databricks:spark-avro_2.11:4.0.0 AVRO_Parquet.py
+#Run script by using:
+#spark-submit --packages mysql:mysql-connector-java:5.1.38,com.databricks:spark-avro_2.11:4.0.0 AVRO_Parquet.py
+# columns 4 = the_day
+# columns 5 =
+spark = SparkSession.builder.getOrCreate()
 
-# def main():
-#
-#     if df.the_day == "Saturday":
-#         df.withColumn("weekend_sales")
-#
-#     elif df.the_day == "Sunday":
-#
-#         else df.withColumn("weekday_sales")
-#
-# if __name__ == '__main__':
-#     spark = SparkSession.builder.getOrCreate()
-#
-#     df = spark.read.load("[DIRECTORY]").
-#
-#     main()
+df = spark.read.load("/home/Yusuf/joined_parquet")
+#df.show()
+groupedDf = df.groupBy("region_id","promotion_id","the_year","the_month").agg({'store_sales': 'sum'})
+
+
+print("\n\n\n\n\n" + groupedDf.show() + "\n\n\n\n\n")
