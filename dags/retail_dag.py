da1a00458aeff3d47104217d958e27c3a34ebd9c
Yusuf Qedan <yusuf9191@gmail.com>
2018-12-28 16:01:41 -0800

    refactor the dag

--- dags/retail_dag.py
+++ dags/retail_dag.py
@@ -1,7 +1,9 @@
 import airflow
+
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+from airflow.contrib.operators.dataproc_operator import DataProcPySparkOperator
+from datetime import timedelta
 
 # To run entire dag:
 # shell> airflow resetdb
@@ -13,34 +15,37 @@ from datetime import datetime, timedelta
 # shell> airflow webserver -p 9990
 # shell> airflow test retail_dag initial_load <today's_date>
 
+# To get these airflow libraries for pycharm run this in windows shell
+# pip install apache-airflow --no-deps
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(1),
+    'start_date': airflow.utils.dates.days_ago(1)
 }
 
 dag = DAG(
     'retail_dag',
     default_args=default_args,
     description='A simple retail DAG',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1)
 )
 
 t1 = BashOperator(
     task_id='initial_load',
-    bash_command= "PYSPARK_PYTHON=python3 /usr/lib/spark/bin/./spark-submit --packages mysql:mysql-connector-java:5.1.38,com.databricks:spark-avro_2.11:4.0.0 /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/InitialLoads.py ",
+    bash_command= "spark-submit --packages mysql:mysql-connector-java:5.1.38,com.databricks:spark-avro_2.11:4.0.0 /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/InitialLoads.py ",
     dag=dag
 )
 
 t2 = BashOperator(
     task_id='avro_parquet',
-    bash_command= "PYSPARK_PYTHON=python3 /usr/lib/spark/bin/./spark-submit --packages com.databricks:spark-avro_2.11:4.0.0  /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/AVRO_Parquet.py ",
+    bash_command= "spark-submit --packages com.databricks:spark-avro_2.11:4.0.0  /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/AVRO_Parquet.py ",
     dag=dag
 )
 
 t3 = BashOperator(
     task_id='parquet_agg',
-    bash_command= "PYSPARK_PYTHON=python3 /usr/lib/spark/bin/./spark-submit /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/Parquet_Agg.py ",
+    bash_command= "spark-submit /mnt/c/Users/Yusuf/PycharmProjects/RetailCaseStudy/Parquet_Agg.py ",
     dag=dag
 )
 
