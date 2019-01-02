e5c966fa37a3201427ce3905fa23c530f0c129a5
Yusuf Qedan <yusuf9191@gmail.com>
2019-01-01 20:08:47 -0800

    add logic to save csv to snowflake table

--- save_csv_to_snowflake.py
+++ save_csv_to_snowflake.py
@@ -1,32 +1,39 @@
 import snowflake.connector
 import boto3
+from boto3 import Session
 
 client = boto3.client('s3')
 resource = boto3.resource('s3')
 bucketName = "yusufqedanbucket"
 bucket = resource.Bucket(bucketName)
+aws_credentials = Session().get_credentials().get_frozen_credentials()
 
 
-def read_credentials_from_s3():
+def read_snowflake_credentials_from_s3():
     for obj in bucket.objects.all():
         if obj.key == "trg/snowflake_credentials":
             out = str(obj.get()['Body'].read())[2:-3].split(',')
             return out
 
 
-output = read_credentials_from_s3()
+output = read_snowflake_credentials_from_s3()
 
-# Gets the version
-ctx = snowflake.connector.connect(
+con = snowflake.connector.connect(
     user=output[0],
     password=output[1],
     account=output[2]
 )
-cs = ctx.cursor()
-try:
-    cs.execute("SELECT current_version()")
-    one_row = cs.fetchone()
-    print(one_row[0])
-finally:
-    cs.close()
-ctx.close()
+
+con.cursor().execute("USE FOOD_MART_AGG")
+
+con.cursor().execute("""
+COPY INTO sales_agg FROM s3://""" + bucketName + "/trg/final_csv" """
+    CREDENTIALS = (
+        aws_key_id='{aws_access_key_id}',
+        aws_secret_key='{aws_secret_access_key}')
+    FILE_FORMAT=(field_delimiter=',')
+""".format(
+    aws_access_key_id=aws_credentials.access_key,
+    aws_secret_access_key=aws_credentials.secret_key))
+
+con.close()
